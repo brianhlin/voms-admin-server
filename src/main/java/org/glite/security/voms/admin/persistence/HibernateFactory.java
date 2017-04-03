7e41a9436dce02cc4bded2e5d3ace4c080b61722
Brian Lin <brianhlin@gmail.com>
2017-04-03 15:39:54 -0500

    Trying ImprovedNamingStrategy

--- src/main/java/org/glite/security/voms/admin/persistence/HibernateFactory.java
+++ src/main/java/org/glite/security/voms/admin/persistence/HibernateFactory.java
@@ -34,7 +34,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.DefaultNamingStrategy;
+import org.hibernate.cfg.ImprovedNamingStrategy;
 
 public class HibernateFactory {
 
@@ -62,7 +62,7 @@ public class HibernateFactory {
                         log.info("before add prop ");
 			hibernateConf.addProperties(conf.getDatabaseProperties());
                         log.info("before setting naming strategy");
-                        hibernateConf.setNamingStrategy(DefaultNamingStrategy.INSTANCE);
+                        hibernateConf.setNamingStrategy(ImprovedNamingStrategy.INSTANCE);
                         log.info("before configure");
 			sessionFactory = hibernateConf.configure().buildSessionFactory();
                         log.info("after configure");
